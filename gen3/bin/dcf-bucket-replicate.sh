5b818176dd35e24718fdfbc3b75c41dcbeb92062
= <=>
2021-03-17 12:59:49 -0500

    point to correct quay image

--- gen3/bin/dcf-bucket-replicate.sh
+++ gen3/bin/dcf-bucket-replicate.sh
@@ -63,7 +63,7 @@ EOM
 
   cat << EOF > ${prefix}-job-definition.json
 {
-    "image": "quay.io/cdis/object_copy:master",
+    "image": "quay.io/cdis/dcf-aws-bucket-replicate:dcf-bucket-duplicate:batch_jobs/dcf_bucket_replicate/object_copy_job.sh",
     "memory": 256,
     "vcpus": 1,
     "environment": [
