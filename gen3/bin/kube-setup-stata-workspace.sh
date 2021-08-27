3b0aae26da3269a189275d9add16e989046045b4
Matthew Cannalte <mcannalte@uchicago.edu>
2021-08-27 14:53:24 -0500

    feat(stata):

--- gen3/bin/kube-setup-stata-workspace.sh
+++ gen3/bin/kube-setup-stata-workspace.sh
@@ -1,3 +1,5 @@
+source "${GEN3_HOME}/gen3/lib/utils.sh"
+gen3_load "gen3/lib/kube-setup-init"
 
 g3kubectl apply -f "${GEN3_HOME}/kube/services/stata/stata-deploy.yaml"
 g3kubectl apply -f "${GEN3_HOME}/kube/services/stata/stata-service.yaml"
